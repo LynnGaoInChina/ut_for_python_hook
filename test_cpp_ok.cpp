61ea3584fe0c9d740a5c54738985d33989a73f57
shengtao.gao <shengtao.gao@pulintech.com>
2021-11-14 07:18:05 +0800

    add cpp test

---
+++ test_cpp_ok.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+   // Say Hello five times
+   for (int index = 0; index < 5; ++index)
+      cout << "HelloWorld!" << endl;
+   char input = 'i';
+   cout << "To exit, press 'm'" << endl;
+   while(input != 'm') {
+      cin  >> input;
+      cout << "You just entered " << input
+           << " you need to enter m to exit." << endl;
+   }
+   exit(0);
+}
